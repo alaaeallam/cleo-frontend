46efbef898cd3780ebd931f75a420cc6d98e0454
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2022-05-25 13:25:52 +0200

    enable login

--- src/component/cart/Cart.js
+++ src/component/cart/Cart.js
@@ -20,12 +20,12 @@ const Cart = () => {
     state: { userInfo },
   } = useContext(UserContext);
 
-  // const handleOpenLogin = () => {
-  //   if (router.push("/?redirect=/checkout")) {
-  //     toggleCartDrawer();
-  //     setModalOpen(!modalOpen);
-  //   }
-  // };
+  const handleOpenLogin = () => {
+    if (router.push("/?redirect=/checkout")) {
+      toggleCartDrawer();
+      setModalOpen(!modalOpen);
+    }
+  };
 
   const checkoutClass = (
     <button
@@ -93,9 +93,13 @@ const Cart = () => {
             checkoutClass
           ) : (
             <span>
-              <Link href="/checkout">
-                <a>{checkoutClass}</a>
-              </Link>
+              {!userInfo ? (
+                <div onClick={handleOpenLogin}>{checkoutClass}</div>
+              ) : (
+                <Link href="/checkout">
+                  <a>{checkoutClass}</a>
+                </Link>
+              )}
             </span>
           )}
         </div>
