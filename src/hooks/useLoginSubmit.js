fa77e616e6b942831a676bec5bbbc07162da1076
Alaa Allam <alaaallam@Alaas-MacBook-Pro.local>
2022-05-25 14:21:21 +0200

    new update for login2

--- src/hooks/useLoginSubmit.js
+++ src/hooks/useLoginSubmit.js
@@ -1,65 +1,108 @@
-import Link from "next/link";
-import React from "react";
-import { FiMail } from "react-icons/fi";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { GoogleLogin } from "react-google-login";
 
 //internal import
-import Error from "@component/form/Error";
-import InputArea from "@component/form/InputArea";
-import useLoginSubmit from "@hooks/useLoginSubmit";
+import UserServices from "@services/UserServices";
+import { UserContext } from "@context/UserContext";
+import { notifyError, notifySuccess } from "@utils/toast";
 
-const ResetPassword = ({ setShowResetPassword, setModalOpen }) => {
-  const { handleSubmit, submitHandler, register, errors, loading } =
-    useLoginSubmit(setModalOpen);
+const useLoginSubmit = (setModalOpen) => {
+  const router = useRouter();
+  const { redirect } = router.query;
+  const { dispatch } = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
 
-  return (
-    <>
-      <div className="text-center mb-6">
-        <Link href="/">
-          <a className="text-3xl font-bold font-serif">Forget Password</a>
-        </Link>
-        <p className="text-sm md:text-base text-gray-500 mt-2 mb-8 sm:mb-10">
-          Reset Your Password
-        </p>
-      </div>
-      <form
-        onSubmit={handleSubmit(submitHandler)}
-        className="flex flex-col justify-center"
-      >
-        <div className="grid grid-cols-1 gap-5">
-          <div className="form-group">
-            <InputArea
-              register={register}
-              label="Email"
-              name="verifyEmail"
-              type="email"
-              placeholder="Your Register Email"
-              Icon={FiMail}
-            />
-            <Error errorName={errors.verifyEmail} />
-          </div>
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
-          <div className="flex items-center justify-between">
-            <div className="flex ms-auto">
-              <button
-                type="button"
-                onClick={() => setShowResetPassword(true)}
-                className="text-end text-sm text-heading ps-3 underline hover:no-underline focus:outline-none"
-              >
-                Forgot password?
-              </button>
-            </div>
-          </div>
-          <button
-            disabled={loading}
-            type="submit"
-            className="w-full text-center py-3 rounded bg-emerald-500 text-white hover:bg-emerald-600 transition-all focus:outline-none my-1"
-          >
-            Recover password
-          </button>
-        </div>
-      </form>
-    </>
-  );
+  const submitHandler = ({
+    name,
+    email,
+    registerEmail,
+    verifyEmail,
+    password,
+  }) => {
+    setLoading(true);
+    if (registerEmail && password) {
+      UserServices.userLogin({
+        registerEmail,
+        password,
+      })
+        .then((res) => {
+          setLoading(false);
+          setModalOpen(false);
+          router.push(redirect || "/");
+          notifySuccess("Login Success!");
+          dispatch({ type: "USER_LOGIN", payload: res });
+          Cookies.set("userInfo", JSON.stringify(res));
+        })
+        .catch((err) => {
+          notifyError(err ? err.response.data.message : err.message);
+          setLoading(false);
+        });
+    }
+    if (name && email && password) {
+      UserServices.verifyEmailAddress({ name, email, password })
+        .then((res) => {
+          setLoading(false);
+          setModalOpen(false);
+          notifySuccess(res.message);
+        })
+        .catch((err) => {
+          setLoading(false);
+          notifyError(err.response.data.message);
+        });
+    }
+    if (verifyEmail) {
+      UserServices.forgetPassword({ verifyEmail })
+        .then((res) => {
+          setLoading(false);
+          notifySuccess(res.message);
+          setValue("verifyEmail");
+        })
+        .catch((err) => {
+          setLoading(false);
+          notifyError(err ? err.response.data.message : err.message);
+        });
+    }
+  };
+
+  const handleGoogleSignIn = (user) => {
+    UserServices.signUpWithProvider({
+      name: user.profileObj.name,
+      email: user.profileObj.email,
+      image: user.profileObj.imageUrl,
+    })
+      .then((res) => {
+        setModalOpen(false);
+        notifySuccess("Login success!");
+        router.push(redirect || "/");
+        dispatch({ type: "USER_LOGIN", payload: res });
+        Cookies.set("userInfo", JSON.stringify(res));
+      })
+
+      .catch((err) => {
+        notifyError(err.message);
+        setModalOpen(false);
+      });
+  };
+
+  return {
+    handleSubmit,
+    submitHandler,
+    handleGoogleSignIn,
+    register,
+    errors,
+    GoogleLogin,
+    loading,
+  };
 };
 
-export default ResetPassword;
+export default useLoginSubmit;
